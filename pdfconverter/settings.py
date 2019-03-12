90ddc5d4d5ad571b052c40b5d0d51d06f9b59cfa
tolu <abiodun.toluwanii@gmail.com>
2019-03-12 05:09:09 -1100

    'static-update'

--- pdfconverter/settings.py
+++ pdfconverter/settings.py
@@ -134,6 +134,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 API_SECRET='OzUJvmJf1jVlhO6Y'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
