2ee9213c2314310bb24ebcac1c85ec258a3af726
tolu <abiodun.toluwanii@gmail.com>
2019-03-18 09:46:06 -1100

    'added-txt'

--- pdfconverter/settings.py
+++ pdfconverter/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$s8y6k9zbbwm(bp1($woe^rm7mpqrhzp+i)8+3x455s^*+4cu#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.projecttopics.org', 'localhost', '127.0.0.1']
 
